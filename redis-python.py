9c585a91983583ef4c46fe45eb6f923ac75ffcaf
Charles Wang <charleswang007@gmail.com>
2022-06-09 16:36:57 +0800

    add redis python example

---
+++ redis-python.py
@@ -0,0 +1,27 @@
+import redis
+
+myHostname = ""
+myPassword = ""
+
+r = redis.StrictRedis(host=myHostname, port=6380,
+                      password=myPassword, ssl=True)
+
+result = r.ping()
+print("Ping returned : " + str(result))
+
+for i in range(100):
+    key = "test"+str(i)
+    val = i+10000
+    r.set(key,val)
+    print("set " + key + " to " + str(val))
+
+#result = r.set("Message", "Hello!, The cache is working with Python!")
+#print("SET Message returned : " + str(result))
+
+#result = r.get("Message")
+#print("GET Message returned : " + result.decode("utf-8"))
+
+result = r.client_list()
+print("CLIENT LIST returned : ")
+for c in result:
+    print("id : " + c['id'] + ", addr : " + c['addr'])
